f26c7450d7e6e2cad82a7ee8197bb47d7a55e313
Vinay DS <vinay.ds21@gmail.com>
2017-03-21 01:00:00 +0530

    dashboard updated

--- app/constants/header.js
+++ app/constants/header.js
@@ -8,6 +8,7 @@ import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import {hashHistory, Link} from 'react-router';
 import Drawer from 'material-ui/Drawer';
+import RemoveRedEye from 'material-ui/svg-icons/image/remove-red-eye';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
 export default class Header extends React.Component{
@@ -34,9 +35,11 @@ export default class Header extends React.Component{
                                         <div>Welcome, {userName}</div>
                                       </div>}
                   />
-                  <Drawer open={this.state.open}>
-                    <MenuItem onClick={()=>{this.setState({open: !this.state.open})}}>Collapse</MenuItem>
-                    <MenuItem>Menu Item 2</MenuItem>
+                  <Drawer open={this.state.open} style={{fontSize:'18px', fontFamily:'Roboto-Light'}}>
+                    <AppBar title={<span style={{fontSize:'18px', fontFamily:'Roboto-Light', color:'white'}}>Main Menu</span>} iconElementLeft={<div></div>}/>
+                    <MenuItem onClick={()=>{this.setState({open: !this.state.open})}} leftIcon={<RemoveRedEye />}>Collapse</MenuItem>
+                    <MenuItem >Profile</MenuItem>
+                    <MenuItem onClick={this._logout.bind(this)}>Log Out</MenuItem>
                   </Drawer>
                 </div>
         );
